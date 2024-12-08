8f2cd263d7de82b6da82badc944bbbffbc8ae25c
vyusalb@inbox.ru <vyusalb@inbox.ru>
2024-12-08 23:49:50 +0300

    Creating variables color, component Button.tsx and creating for her Button.module.scss

---
+++ src/react/shared/Button/ui/Button.tsx
@@ -0,0 +1,41 @@
+import {memo, ReactNode} from "react";
+
+export enum ButtonTheme {
+    DEFAULT = 'defaultTheme',
+    GRADE = 'gradeTheme',
+}
+export enum ButtonSize {
+    SMALL = "buttonSmall",
+    BIG = "buttonBig"
+}
+interface ButtonProps {
+    id: string;
+    children: ReactNode;
+    size?: ButtonSize;
+    theme?: ButtonTheme;
+    disabled?: boolean;
+    onClick?: () => void;
+}
+
+export const Button = memo((props: ButtonProps) => {
+
+    const {
+        children,
+        size = ButtonSize.BIG,
+        theme = ButtonTheme.DEFAULT,
+        disabled,
+        onClick,
+        id
+    } = props;
+
+    return (
+        <button
+          type={'button'}
+          id={id}
+          disabled={disabled}
+          onClick={onClick}
+        >
+            {children}
+        </button>
+    )
+})
